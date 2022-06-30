7110c527b1bdf7f5969bedadb7856f662013dbb5
AlekhyaPathipati <106488346+AlekhyaPathipati@users.noreply.github.com>
2022-06-30 20:01:05 +0530

    Add files via upload

---
+++ Models/Coin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace CryptoCurrency.Models
+{
+    [Keyless]
+    [Table("Coin")]
+    public partial class Coin
+    {
+        public int? Id { get; set; }
+        [StringLength(200)]
+        public string? Name { get; set; }
+        [Column(TypeName = "decimal(20, 2)")]
+        public decimal? Price { get; set; }
+    }
+}
